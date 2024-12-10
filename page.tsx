d51523f34e0345a57d82baa3f088ca0d4bee3205
Sakshi Bhatkar <78723302+Sakshi-Bhatkar@users.noreply.github.com>
2024-12-10 12:13:43 +0530

    Commit 2: update marks
    const [marks, setMarks] = useState(70) and on button create onClick function onClick={ () => { setMarks(90)  } }

--- page.tsx
+++ page.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+   const [marks, setMarks] = useState(70)
+   
   return (
-    <div>
-      page
+   
+    <>
+    <div className='text-blue-800 font-bold text-3xl'>My marks were {marks}
     </div>
+    <button onClick={()=>{setMarks(90)}}
+    className='bg-green-500 px-5 py-2 rounded-2xl m-5 font-bold'>Update</button>
+    </>
   )
 }
 
