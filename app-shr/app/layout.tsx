1384a1f54b9dde67581db2deec9c83cc303f8dc3
Sakshi Bhatkar <bhatkarsakshi09@gmail.com>
2024-12-11 11:40:49 +0530

    installing axios  and getting Autor name from API fetching on an h1

--- app-shr/app/layout.tsx
+++ app-shr/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({children
 }){
   return (
     <html lang="en">
-      <body>
+      <body className="p-12">
       <Header />
         {children}</body>
     </html>
--- app-shr/app/page.tsx
+++ app-shr/app/page.tsx
@@ -1,11 +1,29 @@
-
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 
 const page = () => {
+  const [Images, setImages] = useState([])
+  const getImages= async()=>{
+   try {
+    const resp =  await axios.get("https://picsum.photos/v2/list")
+    const data=resp.data;
+    setImages(data)
+    console.log(Images);
+   } catch (error) {
+    console.error("Error in image")
+   }
+  }
   return (
     <>
-     
+     <button onClick={getImages} className=' px-5 py-3 mt-5 bg-green-500 text-white font-bold'>Get Images</button>
+     <div className='p-10'>
+      {Images.map((elem,i)=>{
+        return <h1 key={i}>{elem.author}</h1>
+      })}
+
+     </div>
     </>
   )
 }
